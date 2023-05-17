2d6f4400fcdecb9bc10c69c84e5509073323cba6
VedatDaglar <vedatdaglar@protonmail.com>
2023-05-18 01:43:21 +0200

    inputs added

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,11 @@
-const { defineConfig } = require("cypress");
+/* eslint-disable no-unused-vars */
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://practice.cydeo.com/',
     video: false,
-    retries : 1,
+    retries: 1,
     defaultCommandTimeout: 5000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/inputs.cy.js
+++ cypress/e2e/inputs.cy.js
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-const */
 /// <reference types="cypress" />
 
 describe('Input Forms Tests', () => {
@@ -6,5 +7,16 @@ describe('Input Forms Tests', () => {
     cy.visit('/registration_form');
   });
 
-  it('Check different input box fields and verify', () => {});
+  it('Check different input box fields and verify', () => {
+    // fill the form for username and other info
+    cy.get('input[name="firstname"]').type('Mike');
+    cy.get('input[name="lastname"]').type('Brown');
+    cy.get('input[name="username"]').type('ChuckNorris');
+    /**
+     * Math.random(): creates a number between 0 - 1 ~ 0.005678
+     * Math.floor : makes it a whole number
+     */
+    let email = `formtest${Math.floor(100000 + Math.random() * 900000)}@cydeo.com`;
+    cy.get('input[name="email"]').type(email);
+  });
 });
